1b2b57cccac43daf342e7411b6cc2b0c2c217802
HUYNH VAN CHI KHAN <huynhkhan91@gmail.com>
2024-05-19 16:07:18 +0700

    update filter by category

--- FashionStore/HelperClass/SessionExtensions.cs
+++ FashionStore/HelperClass/SessionExtensions.cs
@@ -2,6 +2,36 @@
 
 namespace FashionStore.HelperClass
 {
+    //public static class SessionExtensions
+    //{
+    //    public static void SetObjectAsJson(this ISession session, string key, object value)
+    //    {
+    //        session.SetString(key, JsonSerializer.Serialize(value));
+    //    }
+
+    //    public static T GetObjectFromJson<T>(this ISession session, string key)
+    //    {
+    //        var value = session.GetString(key);
+    //        return value == null ? default :
+    //            JsonSerializer.Deserialize<T>(value);
+    //    }
+    //    public static void SetInt32(this ISession session, string key, int value)
+    //    {
+    //        session.SetString(key, value.ToString());
+    //    }
+
+    //    public static int? GetInt32(this ISession session, string key)
+    //    {
+    //        var value = session.GetString(key);
+    //        if (int.TryParse(value, out var result))
+    //        {
+    //            return result;
+    //        }
+    //        return null;
+    //    }
+    //}
+
+
     public static class SessionExtensions
     {
         public static void SetObjectAsJson(this ISession session, string key, object value)
@@ -12,10 +42,25 @@ namespace FashionStore.HelperClass
         public static T GetObjectFromJson<T>(this ISession session, string key)
         {
             var value = session.GetString(key);
-            return value == null ? default :
-                JsonSerializer.Deserialize<T>(value);
+            return value == null ? default : JsonSerializer.Deserialize<T>(value);
+        }
+
+        public static void SetInt32(this ISession session, string key, int value)
+        {
+            session.SetString(key, value.ToString());
+        }
+
+        public static int? GetInt32(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+            if (int.TryParse(value, out var result))
+            {
+                return result;
+            }
+            return null;
         }
     }
+
 }
 
 
